6be3a0fc39c41ea889f0d64a90dd981379270a71
andrerocker <andre.souza@locaweb.com.br>
2011-08-11 19:26:36 -0300

    first commit

---
+++ code.rb
@@ -0,0 +1,121 @@
+class Google
+  attr_accessor :text_a
+  attr_accessor :text_b
+  attr_accessor :vocabulary
+  
+  def initialize
+    @text_a = %w(xjs vvv rfw sqhjkpw rnbn kgmmp qhnkq bscscpfm ttkgmhd qrhdhj hcwsvpt tfnqhptr ngbzgj zxzcr dtd tsrhsb rzvscwq ztqfq dbqxfr bdrdqtwt ckqxhkg mvzmvbt mxhclw xtgsgww cnjpn pvvxcd xtlxt hwkk dhcwnvln gwhzpg zxsfp dmp xqkqsg jvj ltmjgqq cpq pnnmvv mcrcpmnt blrr jxhplsz bwdfnwbt jwxz wwzhlq npdkmvjw sdqkbzz xtgjzhpp cgpdm dsszz tdclpk npnzq jjrx glf qzjhpqj mprdv gpmtw sjpqjjq kxwm sjspjzl tsgpbcqm xdxkvmxb llv kgdcjcn cvhccj pjqjgq xlhbwt fphdzpmb qtmtfr nrljq dtgd npzfsb vpzr zftksnf dbsqbpm ldx qbm sjw jstpw kmzxg kcnzcpc zbp cwnkq pxqnndzg pbcdpt vzqt sgmbdm zmbm tvfwdx djcsjqsm rcrqs sbgkxszs xcgzttx hxb fzw gsjlfq mgfp rknwlztb mfgjmmfm pldtr jnlk qfkf ntlr pdsxlzs qljllcr qndfvkfx jjwt jwmszk gmdx spkbs wttp rjljt dbhgrn tnxb gngmzp scglgln fwgklzj xmvntf pmwj zjfmn mjmndn xsh bblknlkt prkcp gggvn gsntpst pnfdhjt hdk bdxnb fpvzbmr vgmkzqpz zcgsck njrj plqn slqkckz kdjjqvwr rhxnrwcs jnvnml bgtzgs jxr wrcnnmk vnnvzqj kbcqlrbb wscblfqs zfhcrj hwg pnwzxp rjpmwwb tqg kfgzkzn pccsjll wnr qmh hwhnpbdj wbmjncgr mkdt qkrnxnxf nfs dznpjb plnb blxh kqlkcqb wwjq nmsmxf mfzhn zbqfjlm drnf xzvck gqkzldzw kwhpzmvm tvccdx djdhc lmsh mmzxwtlr mhgkdlbn zhhns phfjdq bxrddrp hcgkv tpfr xtpqk zctjhjp xncqvtj blvxzg ctwrjf cktj rdcdttnz svxvnc qrmt vwng txhx ngfzf szj jpbrgzsc qskft dvhwvrb rcqgnc dsfh cxlgbd xcz klfqnwc xsfsbsfw bnmtr rpz cmglhr jpf qthmmwj nftd xzl phjjml lcbfvsj vjhztxz pvvnmwcf qltbzv rxcn xcqkcjj vxskdg smmrhp hphglf gvthr mmj srxbvrwr phqc gfz zfb jptqd sjmjs ddlbn kxp mkknw cvccrt hhzs znv fttvsg tmqnhpfq wlk dhcjzhgf phb lnjzjf bljhtq rbmtdksf qlcp wdxbkknj xhjj cxslpcg mjklxl dhqk xnqkb hmpjpvgv qkgxfwmq vhczkmb nqhnb tjx tdgx xpfmj zhsdvvjl jxzjqf rvb mbjgr jljxnfb lxdkmzxx phcpddh rvqvtd lbpk jtzcc njsq nfmd vlpql zqpknwdx zgg dkw cxxhczml slcgddck srzk qqn zscsmnf qccrc kjnhmt dgmkhbb gsbdkm kqngt qgskdkkt xnxdps zbpvh qhkqm njcm nbj skj qmq gjjtzg ltkz wfptgprx bpfdsmft lqmbvnfz fnmgbzh cqdcjcpm hmcxr gwxcscqg ctqlls dvxx tqflmwlp rsmt djhkmz vmw frsqzcpb wwq vwlj mltv gnx sqwg nhq tmx kxnkhb fmwd pqh dhcbwhgj wpmll clm rbwdz jdckv wvnnm vqjffmks knmwrdv zrpjl mvvtx dtr zph qck kcrjwbrx xmbscmk ffrz tvqqqv rdl kncgvf fcjxfdh mdtx qqc qqkwvpkm hlhcg skm vchkr gpgglb bfzwn mzjhzk dfbwvvf cbbjjjb frtml xgrfjbln pmbc vbh bqkjshnz dczj kjrx gchnkjvm whpjds lxxvc zhwngc cmdrs rhsc bnbt wwjktz jhnsxvk zjcmfdj lxtxwrz gfdzqdcw bbzbnfr wqlcppcw qlmbvgj szpb ffhqcgt gfw whmfpdx xdcngmf gmvbq lgfcm ffqjbm rzqlhlrv vlsvqsxn tbkvt dlxvmbxx fqwb rzzggsfc wkmfq dwzqkqwm rwdwmsm ntq lns whczq twv wnt sffh hdwk xqkpzv jbwbpk xnvm gbkkrcv xftp tgtlt tvwkfmd mhf bxc vxtfvn rlhjnxj vtcssd xgzw tzwgbm sblrqc fbgppcb pdrxlkcn lvwc rdr dsg hrsvnvpb vngm mpvmzg xnf btlvmzgv xjtvtpt zptcc qhfz qrndmslp gdps xxmvzzvk hvqs jtd kvph nzdwph hwr qwb rztlwq xlhsjm gqgkjgb jwtkr rct nkscdmjw gkrbm llbnxlz kqp ctpngjrz zpt nnpjzv vwtbsttf cxfsk kljlz sjx vvjxpjjm lrkr bhkwvzv rqkmtr fwpsdvp wpvfkwd nhvfffwb bckwp tvvzxw vfd hlxfsrr csgb tkvpx pnnwsct nbs vwcwdjf rrqk wsllzvv pzxvw tzb njg vxb zpnbzwr vvtrvcz thxfxzdc glbzfmz vwwnqgb gkbjfpz zfm hvfcp bmh tgkt sslznb fcrkqmk bztzlwz snzw lmvsfhsl wnw xrqz ftmbd fbtm mkgvvvt zqmhhms lnzwmqhz mwvb scbn kcpfxm pnvvlkl vcvgzmzv qvrdpq gtl rfvkfxd mxq ksvtcp dswx ppsmw wwkbj zsgpb jxgh jlrhthz sbdbsxpj nkncstf pvn dgtxh kgfmwn knvcf shsdj tjq mrmltv jwf wstktq fckzqh jpfw jdxzjq frqzwmw dxhsws qpzs gcjcnfq xqcxvq vsnkfk sbsc dhhqnrzx vtxb vvjnhs kwlpdp qdjnq dtqd rjk klm tgvnwwzv gwmjvv rxv dmn dcf btbft mplf cszld zjs tfzswth sjbp kvjhs khjqngl wpbntsgh fkmwrzrc pcmvc sdhtjzq fcn jpsbk csrhn blbbzvhz nwbswgn nmjjdjzs lgns lwfqbncs hxfqn wsrpn cprbbq vkkj lhzpv gnbl jvfb fwd hfzn hfcmnwhc dbvg bvztn qqvcv rfw fqzppgz rlftgbgb xjh lqvqflvr vbdch zmlfjhd vzqldkvd rhbppj tgvsn zsdk gsjqwrnz gbvdsssc rpvz wsgpkxvb kjtsnbh gbxz lmcbnc hcdvjct nstzj rqrhldxs bqd kgjtt whhlxwtw zkkjvgnr rnscb gjmdrr nknrcc dfn kms)
+    @text_b = %w(bml wqtdv kdtdxbqf tdbwkv ltdpgl wjhqrjl lzwnnz vvvqn lrzzwd xrjbxn kcs cvcfbr jhlth kjvqp zgktccnl vcp pqnxt pqcr zzm ksdnzs nfrg xlbgq wtqjwscx hkkztbjg ljw klhkl wvdsjjh hhnx tzwwfrk prfm jsbgx rnpdsrgz ljtd htzdwdg fxthn hngg svzwjpst xnhgclcc kqgzqslm dctbtjb nkvp rbl vqvg lbngqxnp cfpc dbjqndcx qkgstx nmmwlpx jdl lbp gjdplfr cqqx rbmfwt bjzbsc lfhfrzl jxsqcj skdzsfv wfslxvp hsdmmk hhln jwccfvmf brld wlqlsvhf lvjrrv blxmnc zhm sncx ftwtggtj mwvzv hmckdv grmtrmms vdslw hllkg kjmsdvt zztnw lxgw bnvrmjlp vmxdz pdqktrjv hlvzstxl pbfrxcjd xmllmknd npz zvlxt msgmxh qzxj mvf knn rrw jbbdvwh cjndwz smzqj fwdcfsbm zsw xxhcc txjjmzc hkdslw fln hzgjhhlz rkgzmtmw xcv nvdbndxw pgtkcd lhdq lnlqr mdnmdk tlftsq rjqbv sddqhsgt pccntb klpq gvrkxfr fzvppkc hklsgbk brwxr vsndtrv hmkdd dqj kmtt cbwxw rxfd znkfpbqt zrbfxc bxgftfp xwgqq rdgpwrq mhwpmhcg nskks frs slsxnlv tmkjwn rvrmvpxb cvv wzrn ggvrt sdxmtl gch wzqqd zchqtds xtbqznft kltsw txbxc rnkp pmnhs xzljjz mcwlhlm ksnpvrm zxll pjl rbxl mzvwwm qgszjgrd qxp jpndvtqj scdvlq hklpzfl vhhgbthw gtjgv dhx svnlrql lqwvfss ldndx bxbzx tzpzjcg lrl svjhdgzp xkhwd mqd tmsst fltdzk bgqdh chsvjbj dnw nst wzmtr jkwpxwlc zvcwbrk nclclvj lkxk kmm vlgmp kphtxtq drkc vncr brcs glhr gmjt fcggs bmhvfvq vbbjhdt llpjqsck qgqdns gstk zzzj cst vjbcthkb xzhsrqln xzp nkpsgf szbs hwhc slzftpgb bzcrn fxxvtw rtd swxpq zqk qwbh mmb zxkxgp bcfcp bxnhkvnm hkghn dlrqp hqzhzjsn dbz kdr rqscp fpn pct mxdtg pqxzctb qjbrhw lpbbdqj ptrpjjdk knlpv hbkmqjf zhhvkhnm cxpbmtpq fvdd nqxq wcpp tnrxh kgjcvtcd vmfq pznzjpl gqvv xmqskmvl zkjxqnk tsxj ddnppxd mznt fznzn zkpqkf zqtpsxrw zzsnfcp psjc cxdx dtzktp cvvqk mpldqdhh dndw rprkwrqg ppkdbcvw txqqnjbv vcjjrg gpggh vdvnvj fgpdkr kczkn ggfcr bpq fmhpm lwtps bzxz pmgwj jvcncjhc bjznmwjq ghtmvqv wdxk dgbn pxlm dkh hjlhbwrx vmktwpk fgsvt jmvbb lvm xtzkhb sbklhs rsxdph njvgwz nlw ghqxpnmr vsgq wrzn lcg hcvj fdsq hdfthrsd zthqx ftlf rfgcsbc qtzvhn wfhd cnfnfpb vqkq vfnrf kmjnmv njftcc dgrdbpx psxccgf jdzlgn wsdbtssc wtg jfnj kvlftqs bkcfspbn jsgr ptzcrql nzrrcc kgss fzdxv bjdnplql gjvsssxz zlbmjvn cpggwk wrzknp ppcnsrm mtx kqnqbh nnvjjd xzgxz rrcz fgh xvpt rhqcpnb djszd ldqwtq xcskd wmtvfkng ntb rmsv cvhkskcz vgjx mmpcs zlpvpz tvbrkbt tvnklgzc frnjm knb gsfwksx rbjxxxbw hbc bkvr xlc gdwrhvtf rfvzwk nfs kwh whwcrlz nrngzbws zpkhjmpm ngmzzlc gznl vsptksps jzbqmbb bvqhqxjd wskfvg zcf fcgkxtn rjmwq vktlqd ngm lzdhkvvk tqvb hnfh hqnvwr mdmlx grbxh mkd qnvx xtclxbt jbmzhcw vkgqbrm mpn xpdh lwrsskr rfnbdt wnnvs qbbgk vpdznk pct wvh sjqr hzhh zpd frjg wmvt scvssxw fwrj ngds rmvw pxprnnp nqvv vnb zphh qzgptd gsbknrt cmhjzm dxlmlxd wvs dgn jxzxzjh zdfdsh khpnpkkw vtptp cvv wwrjmzsw gbrwfrw qzn cmkdjj jrkp flfhr kqcjf rnczbth mhzx zlrplgw scp nbjcn tcchf npd sbqs xqpd crlddc xsl bzsnzxm xldgkhm nbxbp xbnkvxq qllvfjzh whvxxd srtrrchr fqnnd bctqwvkp lrpl jrs ddgfkk wrkcknwk bnnnkrp xtvbmnm znpkss lvxmvft shwnljsd zdgcjbtp sgcdf swzzcz zjnvs jgscw zlbhld ztqpskxl nrrdx pqdjzkv tlfrrs fhcq lgqrgp rbmw mmmbsvzq rdb nbw hwlqlbj cjj rwwhrh mmh jmdrzn lhmtfq zklcshjk xjxfvktt vdn qpmhs dkcpqph vkfmxz pwvzfr ghqmqls psmxw pqpnjcpr rbj sxmxqw vjczn nqlsrk vrnpc fnwmtdt sjhvcpnr cwkwc bdbczk xktrj rqfzpxsv vtgvnv lclgcpg vwffkzm hlxg ghs zdd cwrsx tcxgmmz dgv zcmxgxr kwpr kbgb xxrf pmmkwqj tqqgtnr sgch wnpjzgw wrpsr pnb rdsmvxwk hrkcpx rpblgq sgwkptjf tmzxwv kqv dldnk nscsr vrbqvplc lbg lwfgfql cxnmxsbx txcf zbjjxsb mgczl nmfz cqjkv ndp lsgnms dht fflhrppg thclc rxmd fqhpshlf ngmdtc lnczrh qcw qlv ctjvql bvqvlvfw cfhn lrhlvkhg qnjrtl wdv rpf wxl fxzw ppfsbvbf xpcs zgkzml vxzc zxtmgvgj czl dsdclcp ngf jwssqfpv znvnx pxhqbh kpszwq dcvfsw bznftpn vbxnsdk nxhbl vxptqw ghtm zpdp sqjtclp wbn dwgntj sldqhjf ntd sckmrbb vlj mqj zkj trsld rltmxf xbc mwmjtsjk mmszwwc qgqlc vfgtnhnp rfwvkkk dksrhhgj xhppjwjf wwl xdqwrc mcczftdl kmjsjg vvw lfs wwsqcl jqwghzk rktl brkxdnv hkmh dthmx lsjggh nvx ztwkdkjz kjw hmp qvbwj zfstcvc)
+    @vocabulary = %w(llv llbnxlz lnzwmqhz lns lnjzjf lqvqflvr lqmbvnfz lgns lgfcm lvwc lcbfvsj lbpk lxxvc lxtxwrz lxdkmzxx lwfqbncs lrkr lhzpv ltkz ltmjgqq lmvsfhsl lmcbnc lmsh ldx nnpjzv nqhnb nkncstf nknrcc nkscdmjw ngbzgj ngfzf nbs nbj nwbswgn nzdwph nrljq nhq nhvfffwb ntlr ntq nstzj nmsmxf nmjjdjzs npnzq npzfsb npdkmvjw nftd nfs nfmd njg njcm njrj njsq qlcp qltbzv qlmbvgj qljllcr qndfvkfx qqn qqkwvpkm qqvcv qqc qkgxfwmq qkrnxnxf qgskdkkt qvrdpq qck qccrc qbm qwb qzjhpqj qrndmslp qrhdhj qrmt qhnkq qhkqm qhfz qthmmwj qtmtfr qskft qmq qmh qdjnq qpzs qfkf klm klfqnwc kljlz knvcf kncgvf knmwrdv kqlkcqb kqngt kqp kgmmp kgdcjcn kgfmwn kgjtt kvph kvjhs kcnzcpc kcrjwbrx kcpfxm kbcqlrbb kxnkhb kxwm kxp kwlpdp kwhpzmvm khjqngl ksvtcp kmzxg kms kdjjqvwr kfgzkzn kjnhmt kjrx kjtsnbh glbzfmz glf gngmzp gnbl gnx gqkzldzw gqgkjgb gkbjfpz gkrbm gggvn gvthr gchnkjvm gcjcnfq gbkkrcv gbvdsssc gbxz gwxcscqg gwhzpg gwmjvv gtl gsntpst gsbdkm gsjlfq gsjqwrnz gmvbq gmdx gdps gpgglb gpmtw gfw gfz gfdzqdcw gjmdrr gjjtzg vlsvqsxn vlpql vnnvzqj vngm vqjffmks vkkj vgmkzqpz vvv vvtrvcz vvjnhs vvjxpjjm vcvgzmzv vchkr vbh vbdch vxb vxtfvn vxskdg vwlj vwng vwcwdjf vwwnqgb vwtbsttf vzqldkvd vzqt vhczkmb vtcssd vtxb vsnkfk vmw vpzr vfd vjhztxz clm cnjpn cqdcjcpm ckqxhkg cktj cgpdm cvccrt cvhccj cbbjjjb cxlgbd cxxhczml cxslpcg cxfsk cwnkq ctqlls ctwrjf ctpngjrz csgb cszld csrhn cmglhr cmdrs cpq cprbbq blvxzg blbbzvhz blxh blrr bljhtq bnbt bnmtr bqkjshnz bqd bgtzgs bvztn bckwp bblknlkt bbzbnfr bxc bxrddrp bwdfnwbt bztzlwz bhkwvzv btlvmzgv btbft bscscpfm bmh bdxnb bdrdqtwt bpfdsmft bfzwn xlhbwt xlhsjm xnqkb xnvm xncqvtj xnxdps xnf xqkqsg xqkpzv xqcxvq xgzw xgrfjbln xcqkcjj xcgzttx xcz xxmvzzvk xzl xzvck xrqz xhjj xtlxt xtgsgww xtgjzhpp xtpqk xsh xsfsbsfw xmvntf xmbscmk xdcngmf xdxkvmxb xpfmj xftp xjh xjtvtpt xjs wlk wnw wnr wnt wqlcppcw wkmfq wvnnm wbmjncgr wwq wwkbj wwzhlq wwjq wwjktz wrcnnmk whczq whhlxwtw whmfpdx whpjds wttp wsllzvv wsgpkxvb wscblfqs wsrpn wstktq wdxbkknj wpvfkwd wpbntsgh wpmll wfptgprx znv zqmhhms zqpknwdx zkkjvgnr zgg zcgsck zctjhjp zbqfjlm zbp zbpvh zxzcr zxsfp zrpjl zhwngc zhhns zhsdvvjl ztqfq zsgpb zscsmnf zsdk zmlfjhd zmbm zpnbzwr zph zpt zptcc zfb zfhcrj zftksnf zfm zjcmfdj zjs zjfmn rlhjnxj rlftgbgb rnbn rnscb rqkmtr rqrhldxs rknwlztb rvqvtd rvb rcqgnc rcrqs rct rbwdz rbmtdksf rxv rxcn rwdwmsm rzqlhlrv rzvscwq rzzggsfc rztlwq rrqk rhbppj rhxnrwcs rhsc rsmt rdl rdcdttnz rdr rpvz rpz rfvkfxd rfw rjljt rjk rjpmwwb hlxfsrr hlhcg hvqs hvfcp hcgkv hcwsvpt hcdvjct hxb hxfqn hwkk hwg hwr hwhnpbdj hrsvnvpb hhzs hmcxr hmpjpvgv hdk hdwk hphglf hfcmnwhc hfzn tnxb tqg tqflmwlp tkvpx tgkt tgvnwwzv tgvsn tgtlt tvqqqv tvvzxw tvccdx tvwkfmd tvfwdx tbkvt txhx twv tzb tzwgbm thxfxzdc ttkgmhd tsgpbcqm tsrhsb tmqnhpfq tmx tdgx tdclpk tpfr tfnqhptr tfzswth tjq tjx slqkckz slcgddck snzw sqwg sqhjkpw skm skj sgmbdm svxvnc scglgln scbn sblrqc sbgkxszs sbsc sbdbsxpj szpb szj srxbvrwr srzk shsdj sslznb smmrhp sdqkbzz sdhtjzq spkbs sffh sjbp sjx sjw sjspjzl sjmjs sjpqjjq mltv mkknw mkgvvvt mkdt mgfp mvvtx mvzmvbt mcrcpmnt mbjgr mxq mxhclw mwvb mzjhzk mrmltv mhgkdlbn mhf mmzxwtlr mmj mdtx mplf mpvmzg mprdv mfgjmmfm mfzhn mjklxl mjmndn dlxvmbxx dkw dgtxh dgmkhbb dvxx dvhwvrb dczj dcf dbqxfr dbvg dbhgrn dbsqbpm dxhsws dwzqkqwm dznpjb drnf dhqk dhcbwhgj dhcwnvln dhcjzhgf dhhqnrzx dtqd dtgd dtr dtd dsg dswx dsszz dsfh dmn dmp ddlbn dfn dfbwvvf djcsjqsm djhkmz djdhc plnb plqn pldtr pnnwsct pnnmvv pnvvlkl pnwzxp pnfdhjt pqh pvn pvvnmwcf pvvxcd pccsjll pcmvc pbcdpt pxqnndzg pzxvw prkcp phqc phcpddh phb phfjdq phjjml pmbc pmwj pdrxlkcn pdsxlzs ppsmw pjqjgq fnmgbzh fqwb fqzppgz fkmwrzrc fcn fckzqh fcrkqmk fcjxfdh fbgppcb fbtm fwgklzj fwd fwpsdvp fzw frqzwmw frtml frsqzcpb fttvsg ftmbd fmwd fpvzbmr fphdzpmb ffqjbm ffrz ffhqcgt jlrhthz jljxnfb jnlk jnvnml jvfb jvj jbwbpk jxgh jxzjqf jxr jxhplsz jwxz jwtkr jwmszk jwf jhnsxvk jtzcc jtd jstpw jdckv jdxzjq jpbrgzsc jptqd jpsbk jpf jpfw jjwt jjrx)
+  end
+end
+
+class String
+  @@googlon_sorter = ":) lnqkgvcbxwzrhtsmdpfj"
+  @@googlon_order = "lnqkgvcbxwzrhtsmdpfj"
+  
+  def <=>(item)
+    self.googlon_order <=> item.googlon_order
+  end
+  
+  def googlon_order
+    base = @@googlon_sorter.size
+    power = self.size
+    
+    each_char.reduce(0) do |sum, value|
+      index = @@googlon_sorter.index(value)
+      power = power - 1
+      sum + (index*base**power)
+    end
+  end
+  
+  def googlon_number
+    base = @@googlon_order.size
+    power = -1
+    
+    each_char.reduce(0) do |sum, value|
+      power = power + 1
+      index = @@googlon_order.index(value)
+      sum = sum + (index*base**power)
+    end
+  end
+end
+
+require "set"
+
+class Array
+  @@foo_letters = "dlgks"
+  
+  def to_googlon_numbers_a
+    map do |value|
+      value.googlon_number
+    end
+  end
+  
+  def items_that(operator, value)
+    select do |item|
+      item = item.size if item.is_a? String
+      item.send(operator, value)
+    end
+  end
+  
+  def ends_with_foo
+    select do |word|
+      @@foo_letters.include? word[word.length-1]
+    end
+  end
+  
+  def starts_with_bar
+    reject do |word|
+      @@foo_letters.include? word[0]
+    end
+  end
+  
+  def without(char)
+    reject do |word|
+      word.include? char
+    end
+  end
+  
+  def beauty
+    select do |number|
+      number % 3 == 0
+    end
+  end
+end
+
+class Set
+  def fill_in_the_blanks
+    max = max_by { |v| v.length }.length
+    map do |word|
+      word.ljust max
+    end
+  end
+end
+
+dev_day = Google.new
+
+puts "- Prepositions"
+puts "--> Text A = #{dev_day.text_a.items_that(:==, 4).ends_with_foo.without("b").size}"
+puts "--> Text B = #{dev_day.text_b.items_that(:==, 4).ends_with_foo.without("b").size}"
+puts 
+
+puts "- Verbs"
+puts "--> Text A = #{dev_day.text_a.items_that(:>=, 6).ends_with_foo.size}"
+puts "--> Text B = #{dev_day.text_b.items_that(:>=, 6).ends_with_foo.size}"
+puts
+
+puts "- Verbs in First Person"
+puts "--> Text A = #{dev_day.text_a.items_that(:>=, 6).ends_with_foo.starts_with_bar.size}"
+puts "--> Text B = #{dev_day.text_b.items_that(:>=, 6).ends_with_foo.starts_with_bar.size}"
+puts
+
+puts "Order"
+`echo #{dev_day.vocabulary.to_set.fill_in_the_blanks.sort} > /tmp/sort.list`
+puts "-- Building ordered list in /tmp/sort.list"
+puts 
+
+puts "Beauty Numbers"
+puts "--> Text A = #{dev_day.text_a.to_googlon_numbers_a.to_set.to_a.items_that(:>=, 906567).beauty.size}"
+puts "--> Text B = #{dev_day.text_b.to_googlon_numbers_a.to_set.to_a.items_that(:>=, 906567).beauty.size}"
+puts
